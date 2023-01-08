bd30a9149a02f97be0a9f12845c23d493394e09d
RigDU\Kadu <kadu@kadu.com.br>
2023-01-08 01:11:22 -0300

    Adicionado controle do led e exemplo

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include <Arduino.h>
+#include "controleled.h"
+
+#define PINODOLED D1
+#define NUMERODELEDS 16
+
+ControlaLed leds(PINODOLED, NUMERODELEDS, NEO_BGR + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(3000);
+  Serial.println("Inicializando!");
+  leds.inicializa();
+
+  Serial.println("Fim Setup");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (size_t i = 0; i < NUMERODELEDS; i++)
+  {
+    leds.limpa();
+    leds.configuraCorDoPixel(i, RED);
+    leds.mostra();
+    delay(1200);
+    leds.configuraCorDoPixel(i, GREEN);
+    leds.mostra();
+    delay(1200);
+    leds.configuraCorDoPixel(i, BLUE);
+    leds.mostra();
+    delay(1200);
+  }
 }
